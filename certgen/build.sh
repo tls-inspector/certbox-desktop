bae18389c3761eae0adc60c65fa6ef9c3ea1239d
Ian Spence <ian@ecnepsnai.com>
2021-02-16 15:17:03 -0800

    Yay go 1.16

--- certgen/build.sh
+++ certgen/build.sh
@@ -3,6 +3,5 @@ set -e
 
 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o certgen_win32_x64.exe
 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o certgen_darwin_x64
-# Enable when Go 1.16 is released
-# GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o certgen_darwin_arm64
+GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o certgen_darwin_arm64
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o certgen_linux_x64
