644b4b952d33dc17f84b7a38e08cd02bd74d8766
Ian Spence <ian@ecnepsnai.com>
2022-02-03 14:12:11 -0800

    Detect arch on deb/rpm compile

--- docker/deb/package_deb.js
+++ docker/deb/package_deb.js
@@ -1,25 +1,24 @@
 const os = require('os');
-const installer = require('electron-installer-debian')
+const installer = require('electron-installer-debian');
 
 const arch_long = os.arch() === 'arm64' ? 'arm64' : 'x86_64';
-const arch_short = os.arch() === 'arm64' ? 'aarch64' : 'x64';
 
 const options = {
-    src: '/build_root/package/Certificate Factory-linux-' + arch_short + '/',
+    src: '/build_root/package/Certificate Factory-linux-' + arch_long + '/',
     dest: '/build_root/package/artifacts/',
     arch: arch_long,
-    icon: '/build_root/package/Certificate Factory-linux-' + arch_short + '/resources/app/dist/assets/img/certificate-factory.png'
+    icon: '/build_root/package/Certificate Factory-linux-' + arch_long + '/resources/app/dist/assets/img/certificate-factory.png'
 }
 
 async function main (options) {
-    console.log('Building .deb package...', options)
+    console.log('Building .deb package...', options);
 
     try {
-        await installer(options)
-        console.log('Finished')
+        await installer(options);
+        console.log('Finished');
     } catch (err) {
-        console.error(err, err.stack)
-        process.exit(1)
+        console.error(err, err.stack);
+        process.exit(1);
     }
 }
-main(options)
+main(options);
--- docker/rpm/package_rpm.js
+++ docker/rpm/package_rpm.js
@@ -2,24 +2,23 @@ const os = require('os');
 const installer = require('electron-installer-redhat');
 
 const arch_long = os.arch() === 'arm64' ? 'arm64' : 'x86_64';
-const arch_short = os.arch() === 'arm64' ? 'aarch64' : 'x64';
 
 const options = {
-    src: '/build_root/package/Certificate Factory-linux-' + arch_short + '/',
+    src: '/build_root/package/Certificate Factory-linux-' + arch_long + '/',
     dest: '/build_root/package/artifacts/',
     arch: arch_long,
-    icon: '/build_root/package/Certificate Factory-linux-' + arch_short + '/resources/app/dist/assets/img/certificate-factory.png'
+    icon: '/build_root/package/Certificate Factory-linux-' + arch_long + '/resources/app/dist/assets/img/certificate-factory.png'
 }
 
 async function main (options) {
     console.log('Building .rpm package...', options);
 
     try {
-        await installer(options)
-        console.log('Finished')
+        await installer(options);
+        console.log('Finished');
     } catch (err) {
-        console.error(err, err.stack)
-        process.exit(1)
+        console.error(err, err.stack);
+        process.exit(1);
     }
 }
-main(options)
+main(options);
