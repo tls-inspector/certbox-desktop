18476290ab0984777e203ead3eb2aa68dd694977
Ian Spence <ian@ecnepsnai.com>
2022-03-22 19:23:21 -0700

    Preserve package name

--- docker/build.sh
+++ docker/build.sh
@@ -10,10 +10,5 @@ cd rpm
 ./build.sh
 cd ../
 
-ARCH='x64'
-if [[ $(uname -m) == "aarch64" ]]; then
-    ARCH='arm64'
-fi
-
-mv "deb/certificate-factory_${VERSION}_${ARCH}.deb" "../package/artifacts/Certificate-Factory_linux_${ARCH}.deb"
-mv "rpm/certificate-factory-${VERSION}-1.${ARCH}.rpm" "../package/artifacts/Certificate-Factory_linux_${ARCH}.rpm"
+mv deb/*.deb ../package/artifacts/
+mv rpm/*.rpm ../package/artifacts/
