0280e1e3549a7d33f9f6bb32ec8e0ff66304d255
Ian Spence <ian@ecnepsnai.com>
2022-10-27 17:43:01 -0700

    Revert "Fix about dialog"
    This reverts commit 14e4a5e802cc52e1f51a0c0685048e1ec573d5dc.

--- src/renderer/components/AboutDialog.tsx
+++ src/renderer/components/AboutDialog.tsx
@@ -1,10 +1,36 @@
 import * as React from 'react';
+import { RuntimeVersions } from '../../shared/types';
 import { IPC } from '../services/IPC';
 import { Link } from './Link';
 import { Dialog } from './Dialog';
 import '../../../css/About.scss';
 
 export const AboutDialog: React.FC = () => {
+    const [Loading, setLoading] = React.useState(true);
+    const [Versions, setVersions] = React.useState<RuntimeVersions>();
+
+    React.useEffect(() => {
+        IPC.runtimeVersions().then(versions => {
+            setVersions(versions);
+            setLoading(false);
+        });
+    }, []);
+
+    const versionContent = () => {
+        if (Loading) {
+            return null;
+        }
+
+        return (
+            <p>
+                Application: <strong>{Versions.app}</strong><br/>
+                Electron: <strong>{Versions.electron}</strong><br/>
+                Node.js: <strong>{Versions.nodejs}</strong><br/>
+                Golang: <strong>{Versions.golang}</strong><br/>
+            </p>
+        );
+    };
+
     return (<Dialog title="About Certificate Factory" buttons={[{label: 'Dismiss'}]}>
         <div className="about">
             <div className="image">
@@ -13,7 +39,7 @@ export const AboutDialog: React.FC = () => {
             <div className="contents">
                 <h1>Certificate Factory</h1>
                 <p>Copyright &copy; <Link url="https://ianspence.com">Ian Spence</Link> 2021-2022. Released under the <Link url="https://www.gnu.org/licenses/gpl-3.0.en.html">GPLv3 license</Link>. Source code available at <Link url="https://github.com/tls-inspector/certificate-factory">github.com/tls-inspector/certificate-factory</Link>.</p>
-                <p>{IPC.packageVersion()}</p>
+                {versionContent()}
             </div>
         </div>
     </Dialog>);
